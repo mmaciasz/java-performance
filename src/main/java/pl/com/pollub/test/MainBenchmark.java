0ec20359a96eb52e4c39d6e7fb0eb51a2e0a571c
root <mr.mati1@wp.pl>
2016-12-31 16:08:17 +0100

    log4j setup

--- src/main/java/pl/com/pollub/test/MainBenchmark.java
+++ src/main/java/pl/com/pollub/test/MainBenchmark.java
@@ -1,6 +1,5 @@
 package pl.com.pollub.test;
 
-
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
 import org.openjdk.jmh.annotations.Mode;
@@ -9,13 +8,27 @@ import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import pl.com.pollub.test.dto.Person;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 /**
  * Created by mmaciasz on 2016-12-07.
  */
 public class MainBenchmark {
+
+    private static final Logger log;
+
+    private static List<Person> persons;
+
+    static {
+        log = LoggerFactory.getLogger(MainBenchmark.class);
+        persons = DataPrepare.prepareData(100000);
+    }
 
     @Benchmark
     @BenchmarkMode(Mode.AverageTime)
@@ -24,12 +37,20 @@ public class MainBenchmark {
         DataPrepare.prepareData(10000);
     }
 
+    @Benchmark
+    @BenchmarkMode(Mode.AverageTime)
+    @OutputTimeUnit(TimeUnit.MILLISECONDS)
+    public void mapTest() {
+        persons.stream().collect(Collectors.toMap(Person::getPersonId, person -> person));
+    }
+
     public static void main(String[] args) throws RunnerException {
+        log.error("Beginning jmh tests...");
         Options opt = new OptionsBuilder()
                 .include(MainBenchmark.class.getSimpleName())
                 .warmupIterations(5)
                 .measurementIterations(5)
-                .forks(2)
+                .forks(1)
                 .build();
 
         new Runner(opt).run();
